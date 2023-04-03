d839bbc95dd7f788392f78320c598db7548f814a
Anmol Dandiwal <dandiwal.anmol@outlook.com>
2023-04-03 13:11:28 -0400

    Finalized KNN classifier, k = 5

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import f1_score, confusion_matrix, classification_report
 import pandas as pd 
 
 def main():
-    kNearestNeighbours()
+    # After checking all values for k from 1-10, 5 was most accurate
+    score = kNearestNeighbours(5)
 
-def kNearestNeighbours():
+def kNearestNeighbours(k):
     url = 'pulsars.csv'
     names = ['att1', 'att2', 'att3', 'att4', 'att5', 'att6', 'att7', 'att8', 'Class']
     dataset = pd.read_csv(url, names=names)
@@ -22,15 +23,14 @@ def kNearestNeighbours():
     att_train = scaler.transform(att_train)
     att_test = scaler.transform(att_test)
 
-    classifier = KNeighborsClassifier()
+    classifier = KNeighborsClassifier(k)
     classifier.fit(att_train, class_train)
 
     predictions = classifier.predict(att_test)
 
     print(confusion_matrix(class_test, predictions))
     print(classification_report(class_test, predictions))
-
-
+    return f1_score(class_test, predictions)
 
 if __name__ == '__main__':
     main()
