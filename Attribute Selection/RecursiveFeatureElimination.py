5dfe6de32f623696bf68f1397168fe5ad50adb2f
Michelle Pham <michellepham@Michelles-Air.ht.home>
2023-04-06 13:54:21 -0400

    Added attribute selection folder

---
+++ Attribute Selection/RecursiveFeatureElimination.py
@@ -0,0 +1,29 @@
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LogisticRegression
+import pandas as pd
+import numpy as np
+
+# Load the dataset
+url = "../pulsars.csv"
+df = pd.read_csv(url, header=None)
+
+X = df.iloc[:, :-1]
+y = df.iloc[:, -1]
+
+model = LogisticRegression()
+
+#Create the RFE model that will select 4 features that are the most important.
+'''
+    Args:
+    model:
+    n_features_to_select:
+    step:
+'''
+selector = RFE(model, n_features_to_select=4, step=5)
+
+# Fit the selector to the data
+selector.fit(X, y)
+selected_features = np.array(df.columns[:-1])[selector.support_]
+
+# Print the selected features
+print("Selected features:", selected_features)
---
+++ Attribute Selection/TreeBasedSelection.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.feature_selection import SelectFromModel
+
+# Load the dataset
+url = "../pulsars.csv"
+df = pd.read_csv(url, header=None)
+
+# Split the dataset into input features and target variable
+X = df.iloc[:, :-1]
+y = df.iloc[:, -1]
+
+# Define the tree-based feature selection model
+clf = ExtraTreesClassifier(n_estimators=100)
+
+# Fit the model and select features
+clf.fit(X, y)
+model = SelectFromModel(clf, prefit=True)
+X_new = model.transform(X)
+
+# Print the selected features
+selected_features = X.columns[model.get_support()]
+print("Selected features:", selected_features)
